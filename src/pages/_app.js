edc178bf80cafa42b87577208b9a0ab1e4c5664d
Simon <fiewind1@gmail.com>
2023-05-23 22:47:33 +0300

    Worked out i18n

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+
+import { appWithTranslation } from 'next-i18next'
+
+const App = ({ Component, pageProps }) => (
+    <Component {...pageProps} />
+)
+
+export default appWithTranslation(App);
---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import Navbar from './components/Navbar';
+
+export default function Home() {
+  const { t } = useTranslation()
+
+  const { locale, locales, push } = useRouter();
+
+  const handleClick = l => () => {
+    push('/', undefined, {locale: l})
+  }
+
+  return (
+    <>
+    <Navbar l={locales} t={t}/>
+     <h1>{t("HELLO_WORLD")}</h1>
+      {locales.map(l => (
+        <button key={l} onClick={handleClick(l)}>
+          {l}
+        </button>
+      ))}
+    </>
+  )
+}
+
+export async function getStaticProps(context) {
+  // extract the locale identifier from the URL
+  const { locale } = context
+
+  return {
+    props: {
+      // pass the translation props to the page component
+      ...(await serverSideTranslations(locale)),
+    },
+  }
+}
