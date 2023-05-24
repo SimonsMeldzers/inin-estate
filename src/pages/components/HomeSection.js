37515b429059b28ae989b499b2583069c108a5f9
Simon <fiewind1@gmail.com>
2023-05-24 18:25:01 +0300

    Made the Decore section of the HomeSection (not yet responsive)

--- src/pages/components/HomeSection.js
+++ src/pages/components/HomeSection.js
@@ -1,6 +1,8 @@
 import { Button, Paper, Typography } from "@mui/material";
 import Link from "next/link";
 
+import EastIcon from '@mui/icons-material/East';
+
 function HomeSection() {
   return (
     <div className="section-container">
@@ -38,7 +40,21 @@ function HomeSection() {
             <Typography id="section-featured-price" variant="subtitle2"> Starting price ~70.000€ </Typography>
             <Button className="section-featured-button" variant="contained"> Make An Offer </Button>
         </Paper>
+      </div>
 
+      <div className="section-decore-container">
+        <div className="section-decore">
+            <div className="section-decore-left">
+                <img src="https://i.ibb.co/qFpk0T8/decore.png" alt="" />
+            </div>
+            <div className="section-decore-right">
+                <Typography id="section-decore-right-text" variant="h4" component='h2'>
+                “We specialize in crafting unique custom houses tailored to your needs. Our team of skilled professionals take pride in producing high-quality work that is unmatched in the industry!,,
+                </Typography>
+                <Typography id="section-decore-right-inin" variant="h6" component='p'> - InIn estate </Typography>
+                <Button className="section-decore-right-button" variant="contained"> Learn More <EastIcon fontSize="small" sx={{ml:1}}/> </Button>
+            </div>
+        </div>
       </div>
     </div>
   );
