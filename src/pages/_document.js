f53752bccace0ff0438d2a5a4d3e5a8551d65b0b
Simon <fiewind1@gmail.com>
2023-05-25 20:05:43 +0300

    Added favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="shortcut icon" href="favicon/favicon.ico" />
       <body>
         <Main />
         <NextScript />
