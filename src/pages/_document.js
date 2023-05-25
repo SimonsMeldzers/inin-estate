5158b7c929e717eb654591992f92114ec139315b
Simon <fiewind1@gmail.com>
2023-05-25 20:08:39 +0300

    Added website title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>InIn Estate</title>
       <link rel="shortcut icon" href="favicon/favicon.ico" />
       <body>
         <Main />
